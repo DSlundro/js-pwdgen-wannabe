f14b5ee43a8d8f20d8db2bf0a85cdf09e1146f14
DSlundro <dragan.savic95@outlook.it>
2022-03-23 16:42:48 +0100

    added 1 more part

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,21 @@ const userPassword = userName + userSurname + userColor + 22;
 console.log(userPassword);
 document.getElementById('psw').innerHTML = userPassword;
 
+//                                                  PART 2
 
+//chiedi all'utente il suo nome
+const userName2 = prompt('BETTER PSW: What is your Name?');
+console.log(userName);
 
+//chiedi all'utente il suo cognome
+const userSurname2 = prompt('BETTER PSW: What is your Surname?');
+console.log(userSurname);
 
+//chiedi all'utente il colore preferito
+const userColor2 = prompt('BETTER PSW: What is your favourite color?');
+console.log(userColor);
+
+//risultato finale
+const userPassword2 = 'òA0^' + userName2 + '5548' + userSurname2 + '<^.^>' + userColor2 + 22;
+console.log(userPassword);
+document.getElementById('psw2').innerHTML = userPassword2;
